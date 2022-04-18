bc9515a534a2739b9a8394bea600319d396418ce
Vivek Kumar Gupta <100516335+thewisdomgupta@users.noreply.github.com>
2022-04-18 17:57:20 +0530

    Add files via upload

---
+++ Addition_Two_Number.java
@@ -0,0 +1,9 @@
+public class Addition_Two_Number {
+    public static void main(String [] args){
+        int x=10;
+        int y=20;
+        int total=x+y;
+        System.out.printf("Sum of %d & %d is: %d",x,y,total);
+    }
+}
+//This Code is Contributed by Vivek Kumar Gupta
